8b774bf118bb72c65a6c3891bf2fbbebf2071795
AllanHuang853 <shengqi5@illinois.edu>
2019-12-03 13:45:53 -0600

    updated

--- app/src/main/java/com/example/twentytwenty/CandidateInfo.java
+++ app/src/main/java/com/example/twentytwenty/CandidateInfo.java
@@ -13,7 +13,7 @@ public class CandidateInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidate_info);
         Button voterInfo = (Button) findViewById(R.id.voter_information);
-        //voterInfo.setOnClickListener(unused -> onClick());
+        voterInfo.setOnClickListener(unused -> onClick());
     }
     private void onClick() {
         Intent intent = new Intent(this, VoterInfo.class);
