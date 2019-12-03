e435cbfd728bfed9eec4dd0ecb141a339820fd22
AllanHuang853 <shengqi5@illinois.edu>
2019-12-02 18:28:07 -0600

    still has to connect activities

---
+++ app/src/main/java/com/example/twentytwenty/CandidateInfo.java
@@ -0,0 +1,14 @@
+package com.example.twentytwenty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CandidateInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidate_info);
+    }
+}
